02aa6a771075914aa3d9c76609e3e20fd3bd86a4
Kiryl-Stanevich <56796225+Kiryl-Stanevich@users.noreply.github.com>
2019-10-25 12:05:02 +0300

    Add System.out.print ("Введите радиус R="); Scanner inputFigure = new Scanner (System.in); int r = inputFigure.nextInt (); System.out.print ("Введите координату х="); Scanner inputFigure1 = new Scanner (System.in); int x = inputFigure1.nextInt (); System.out.print ("Введите координату y="); Scanner inputFigure2 = new Scanner (System.in); int y = inputFigure2.nextInt ();
    else if (x == 0 && y == 0) {
    System.out.println("Точка находися в начале координат");
    }
    else if (x == 0) {
    System.out.println("Точка находится на оси 'x'");
    }
    else if (y == 0)  {
    System.out.println("Точка находится на оси 'y'");

--- src/Work1.java
+++ src/Work1.java
@@ -1,10 +1,16 @@
-
+import java.util.Scanner;
 public class Work1 {
     public static void main(String[] args) {
 
-        int r = 5;
-        int x = 2;
-        int y = 1;
+        System.out.print ("Введите радиус R=");
+        Scanner inputFigure = new Scanner (System.in);
+        int r = inputFigure.nextInt ();
+        System.out.print ("Введите координату х=");
+        Scanner inputFigure1 = new Scanner (System.in);
+        int x = inputFigure1.nextInt ();
+        System.out.print ("Введите координату y=");
+        Scanner inputFigure2 = new Scanner (System.in);
+        int y = inputFigure2.nextInt ();
 
         if (Math.abs(x) <= r && Math.abs(y) <= r) {
             System.out.println("yes,");
@@ -19,7 +25,16 @@ public class Work1 {
             System.out.println("III");
         }
             else if (x > 0 && y < 0) {
-                System.out.println("IV");
+            System.out.println("IV");
+        }
+                else if (x == 0 && y == 0) {
+            System.out.println("Точка находися в начале координат");
+        }
+                else if (x == 0) {
+            System.out.println("Точка находится на оси 'x'");
+        }
+                    else if (y == 0)  {
+                        System.out.println("Точка находится на оси 'y'");
                 }
             }
         }
